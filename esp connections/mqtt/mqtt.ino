8173e957b5290b6eada898c9a649b8a1e2bdf991
vihaancp <vihaan@cellpropulsion.com>
2024-10-22 15:37:02 +0530

    mqtt fixes

--- esp connections/mqtt/mqtt.ino
+++ esp connections/mqtt/mqtt.ino
@@ -1,29 +1,56 @@
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-const char *ssid =  "nWO";     // replace with your wifi ssid and wpa2 key
-const char *pass =  "Cp#super123";
+const char* ssid = "nWO";     // Replace with your Wi-Fi SSID
+const char* pass = "Cp#super123";  // Replace with your Wi-Fi password
+const char* mqtt_server = "192.168.68.106";  // Replace with your MQTT server address
 
-WiFiClient client;
- 
-void setup() 
-{
-       Serial.begin(9600);
-       delay(10);
-               
-       Serial.println("Connecting to ");
-       Serial.println(ssid); 
- 
-       WiFi.begin(ssid, pass); 
-       while (WiFi.status() != WL_CONNECTED) 
-          {
-            delay(500);
-            Serial.print(".");
-          }
-      Serial.println("");
-      Serial.println("WiFi connected"); 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1884);
 }
- 
-void loop() 
-{      
-  
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  client.publish("turnstile", "Test content");
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("Connecting to ");
+  Serial.println(ssid); 
+
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected"); 
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe or publish as needed
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
